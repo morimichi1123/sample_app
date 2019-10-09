7aad7cd69116a74cfa879f4afb027d6dd572ed35
morimichi1123 <morisaki.michiya@lmi.ne.jp>
2019-10-09 05:40:43 +0000

    Finish statics pages

--- Gemfile
+++ Gemfile
@@ -25,10 +25,10 @@ end
 
 group :test do
   gem 'rails-controller-testing', '1.0.2'
-  gem 'minitest',                 '5.10.3'
-  gem 'minitest-reporters',       '1.1.14'
+# gem 'minitest',                 '5.10.3'
+# gem 'minitest-reporters',       '1.1.14'
   gem 'guard',                    '2.13.0'
-  gem 'guard-minitest',           '2.4.4'
+# gem 'guard-minitest',           '2.4.4'
 end
 
 group :production do
--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,19 +1,26 @@
 require 'rails_helper'
 
+
+
 RSpec.describe "multi_access", type: :request do
+
+  before do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+  
   it "ホーム・ヘルプ画面に遷移できる" do
 
     get static_pages_home_url
     expect(response.status).to eq 200
-  #  assert_response :success#要書き換え
-
+    assert_select "title","Home | #{@base_title}"
+    
     get static_pages_help_url
     expect(response.status).to eq 200
-  #  assert_response :success#要書き換え
+    assert_select "title","Help | #{@base_title}"
   
     get static_pages_about_url
-  #  assert_response :success#要書き換え
     expect(response.status).to eq 200
+    assert_select "title","About | #{@base_title}"
 
   end
 end
