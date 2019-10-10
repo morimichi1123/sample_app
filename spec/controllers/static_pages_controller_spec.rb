6546751ad25c09dbda215867400bf15eb9ac12ca
morimichi1123 <morisaki.michiya@lmi.ne.jp>
2019-10-10 07:38:23 +0000

    5.3.1DONE

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,23 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe "multi_access", type: :request do
-#  before do
-#    @base_title = "Ruby on Rails Tutorial Sample App"
-#  end
+  before do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
   
   it "ホーム画面などに遷移できる" do
 
-    get static_pages_home_url
+    get root_path
     expect(response.status).to eq 200
     assert_select "title", "Ruby on Rails Tutorial Sample App"
     
-#    get static_pages_help_url
-#    expect(response.status).to eq 200
-#    assert_select "title","Help | #{@base_title}"
+    get help_path
+    expect(response.status).to eq 200
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   
-#    get static_pages_about_url
-#    expect(response.status).to eq 200
-#    assert_select "title","About | #{@base_title}"
+    get about_path
+    expect(response.status).to eq 200
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
 
+    get contact_path
+    expect(response.status).to eq 200
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 end
